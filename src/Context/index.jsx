c393987e89761256b3031c74bc8c02c61778d4e8
riya <rriya_be20@thapar.edu>
2024-04-27 14:45:27 +0530

    frontend commit

---
+++ src/Context/index.jsx
@@ -0,0 +1,65 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from 'axios'
+
+const StateContext = createContext()
+
+export const StateContextProvider = ({ children }) => {
+    const [weather, setWeather] = useState({})
+    const [values, setValues] = useState([])
+    const [place, setPlace] = useState('Delhi')
+    const [thisLocation, setLocation] = useState('')
+
+    // fetch api
+    const fetchWeather = async () => {
+        const options = {
+            method: 'GET',
+            url: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
+            params: {
+                aggregateHours: '24',
+                location: place,
+                contentType: 'json',
+                unitGroup: 'metric',
+                shortColumnNames: 0,
+            },
+            headers: {
+                'X-RapidAPI-Key': '999b0da2e5msh4bbb1cf939fd1f6p1baea2jsnae4e2956200b',
+                'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+            }
+        }
+
+        try {
+            const response = await axios.request(options);
+            console.log(response.data)
+            const thisData = Object.values(response.data.locations)[0]
+            setLocation(thisData.address)
+            setValues(thisData.values)
+            setWeather(thisData.values[0])
+        } catch (e) {
+            console.error(e);
+            // if the api throws error.
+            alert('This place does not exist')
+        }
+    }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [place])
+
+    useEffect(() => {
+        console.log(values)
+    }, [values])
+
+    return (
+        <StateContext.Provider value={{
+            weather,
+            setPlace,
+            values,
+            thisLocation,
+            place
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
